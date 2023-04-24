3dc728cc49ccd2c32d5c0b90123ff5d2fb2cf32b
harshadabhole <harshada.bhole@thinkitive.com>
2023-04-24 12:05:22 +0530

    Added packages and map component changes

--- src/app/components/dashboard/map/map.component.ts
+++ src/app/components/dashboard/map/map.component.ts
@@ -9,18 +9,42 @@ import { SiteService } from 'src/app/shared/services/site.service';
 })
 export class MapComponent implements OnInit {
   loading:boolean=false;
+  
   map: mapboxgl.Map;
   style = 'mapbox://styles/mapbox/streets-v11';
   lat = 37.75;
   lng = -122.41;
-  selectedSite: any;
-  constructor(
-    private _siteService: SiteService,
-  ) { 
-    this._siteService.updatedSiteId.subscribe((res: any) => {
-      this.selectedSite = this._siteService.getselectedSite();
-    })
-  }
+
+  public geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-77.032, 38.913]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Washington, D.C.'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-122.414, 37.776]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'San Francisco, California'
+        }
+      }
+    ]
+  };
+
+  constructor() { }
+  
   ngOnInit() {
     this.loading=true;
     // mapboxgl.accessToken = environment.mapbox.accessToken;
@@ -32,6 +56,23 @@ export class MapComponent implements OnInit {
         center: [this.lng, this.lat]
     });
     // Add map controls
+    
+    for (const feature of this.geojson.features) {
+      // create a HTML element for each feature
+      const el = document.createElement('div');
+      el.className = 'marker';
+
+      // make a marker for each feature and add it to the map
+      new mapboxgl.Marker(el)
+        .setLngLat(feature.geometry.coordinates)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML(
+              `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+            )
+        )
+        .addTo(this.map);
+    }
     this.map.addControl(new mapboxgl.NavigationControl());
     this.loading=false;
   }
