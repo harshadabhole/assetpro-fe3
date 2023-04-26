196dd9e9da949098525488b15525360cf8f96ddb
janhavi.badgujar <janhavi.badgujar@thinkitive.com>
2023-04-26 16:56:26 +0530

    Working on Map

--- src/app/components/dashboard/map/map.component.ts
+++ src/app/components/dashboard/map/map.component.ts
@@ -15,65 +15,109 @@ export class MapComponent implements OnInit {
   lat = 37.75;
   lng = -122.41;
 
-  public geojson = {
-    type: 'FeatureCollection',
-    features: [
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-77.032, 38.913]
-        },
-        properties: {
-          title: 'Mapbox',
-          description: 'Washington, D.C.'
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-122.414, 37.776]
-        },
-        properties: {
-          title: 'Mapbox',
-          description: 'San Francisco, California'
-        }
-      }
-    ]
-  };
+  
 
   constructor() { }
-  
+
   ngOnInit() {
     this.loading=true;
-    // mapboxgl.accessToken = environment.mapbox.accessToken;
-    Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set(environment.mapbox.accessToken);
-      this.map = new mapboxgl.Map({
-        container: 'map',
-        style: this.style,
-        zoom: 13,
-        center: [this.lng, this.lat]
-    });
-    // Add map controls
+    const geojson = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [-77.032, 38.913]
+          },
+          properties: {
+            title: 'Mapbox',
+            description: 'Washington, D.C.',
+            image : '../../../../assets/images/car.jpg'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [-122.414, 37.776]
+          },
+          properties: {
+            title: 'Mapbox',
+            description: 'San Francisco, California',
+            image : '../../../../assets/images/car.jpg'
+          }
+        }
+      ]
+    };
     
-    for (const feature of this.geojson.features) {
-      // create a HTML element for each feature
+    Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set(environment.mapbox.accessToken);
+    //   this.map = new mapboxgl.Map({
+    //     container: 'map',
+    //     style: this.style,
+    //     zoom: 3,
+    //     center: [this.lng, this.lat]
+    // });
+    // // Add map controls
+    // for (const feature of geojson.features) {
+    //   // create a HTML element for each feature
+    //   const el = document.createElement('div');
+    //   el.className = 'marker';
+    //   console.log('ellll', el)
+
+    //   // make a marker for each feature and add it to the map
+    //   new mapboxgl.Marker(el)
+    //     .setLngLat(feature.geometry.coordinates)
+    //     .setPopup(
+    //       new mapboxgl.Popup({ offset: 25 }) // add popups
+    //         .setHTML(
+    //           `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+    //         )
+    //     )
+    //     .addTo(this.map);
+    // }
+
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-122.436, 37.773],
+      zoom: 12
+    });
+
+    map.on('load', () => {
+      for (const feature of geojson.features) {
+      // Add a marker at the center of the map
+      new mapboxgl.Marker()
+        .setLngLat(feature.geometry.coordinates)
+        .addTo(map);
+
+      // Define a custom marker using an HTML element
       const el = document.createElement('div');
       el.className = 'marker';
+      // el.style.backgroundImage = 'url(https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png)';
+      el.style.width = '50px';
+      el.style.height = '50px';
 
-      // make a marker for each feature and add it to the map
+      // Add a custom marker at the Ferry Building in San Francisco
       new mapboxgl.Marker(el)
         .setLngLat(feature.geometry.coordinates)
         .setPopup(
           new mapboxgl.Popup({ offset: 25 }) // add popups
             .setHTML(
-              `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+              `<div style="display:flex; align-items:center;">
+              <img src="${feature.properties.image}" style=" width:50px; margin-right:10px;">
+              <div>
+                <h3>${feature.properties.title}</h3>
+                <p>${feature.properties.description}</p>
+              </div>
+            </div>`
             )
         )
-        .addTo(this.map);
-    }
-    this.map.addControl(new mapboxgl.NavigationControl());
+        .addTo(map);
+      }
+    });
+    
+    // this.map.addControl(new mapboxgl.NavigationControl());
     this.loading=false;
   }
 }
